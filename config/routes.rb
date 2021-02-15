dfc65b7e65b68c5bc442867ce67837e57f473f94
Rukshan Jonny Uddin <rukshan.uddin@gmail.com>
2021-02-14 23:19:42 -0500

    tried caching

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'application#root'
-  get 'api' => 'application#root'
   scope :api do
     match 'players/rankings' => 'player_stats#ranked', via: :get
     # post 'players/:id/images' => 'players/upload_images'
