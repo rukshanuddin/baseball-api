c4eb6a50b12d777e649d49b2b1070d021ad5e87d
Rukshan Jonny Uddin <rukshan.uddin@gmail.com>
2021-02-14 20:04:45 -0500

    most routes functional (working on images)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   scope :api do
+    match 'players/rankings' => 'player_stats#ranked', via: :get
+    match 'players/:id/images' => 'players/upload_images', via: :post
+    # match 'players/:id/images' => 'players/images', via: :get
+    match 'players/:id/videos' => 'players/upload_videos', via: :post
+    # match 'players/:id/videos' => 'players/videos', via: :get
     resources :players
-    get 'player_rankings' => 'players#ranked'
+    resources :player_stats
+    match 'teams/by_wins' => 'teams#wins', via: :get
+    match 'teams/rankings' => 'team_stats#ranked', via: :get
     resources :teams
-    get 'team_rankings' => 'teams#ranked'
+    resources :team_stats
     resources :games
   end
 end
